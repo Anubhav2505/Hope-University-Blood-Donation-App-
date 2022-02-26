f1162679fd7ea2d4ece91312da799c9829b56eb8
Anubhav2505 <Deshwal1010@gmail.com>
2022-02-26 20:01:27 +0530

    Onboarding,Login and Register

---
+++ app/src/main/java/com/example/hope/MainActivity.java
@@ -0,0 +1,143 @@
+package com.example.hope;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager mSLideViewPager;
+    LinearLayout mDotLayout;
+    ImageButton backbtn, nextbtn;
+    Button skipbtn;
+    TextView[] dots;
+    ViewPagerAdapter viewPagerAdapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        backbtn = findViewById(R.id.backbtn);
+        nextbtn = findViewById(R.id.fwdbtn);
+        skipbtn = findViewById(R.id.skipbutton);
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (getitem(0) > 0){
+
+                    mSLideViewPager.setCurrentItem(getitem(-1),true);
+
+                }
+
+            }
+        });
+
+        nextbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (getitem(0) < 2)
+                    mSLideViewPager.setCurrentItem(getitem(1),true);
+                else {
+
+                    Intent i = new Intent(MainActivity.this,mainscreen.class);
+                    startActivity(i);
+                    finish();
+
+                }
+
+            }
+        });
+
+        skipbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(MainActivity.this,mainscreen.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
+        mSLideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.indicatorlayout);
+
+        viewPagerAdapter = new ViewPagerAdapter(this);
+
+        mSLideViewPager.setAdapter(viewPagerAdapter);
+
+        setUpindicator(0);
+        mSLideViewPager.addOnPageChangeListener(viewListener);
+
+    }
+    public void setUpindicator(int position){
+
+        dots = new TextView[3];
+        mDotLayout.removeAllViews();
+
+        for (int i = 0 ; i < dots.length ; i++){
+
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.inactive));
+            mDotLayout.addView(dots[i]);
+
+        }
+
+             dots[position].setTextColor(getResources().getColor(R.color.active));
+
+
+    }
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+            setUpindicator(position);
+
+            if (position > 0){
+
+                backbtn.setVisibility(View.VISIBLE);
+
+            }else {
+
+                backbtn.setVisibility(View.INVISIBLE);
+
+            }
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private int getitem(int i){
+
+        return mSLideViewPager.getCurrentItem() + i;
+    }
+
+
+}
